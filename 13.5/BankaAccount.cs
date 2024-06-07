15287eb7858ec355548ab4138bfca4bc8f431d62
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-06-07 11:52:15 +0300

    Add files via upload

---
+++ 13.5/BankaAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._5
+{
+    internal class BankaAccount
+    {
+        public static double vkaraniPari { get; set; } 
+    }
+}
