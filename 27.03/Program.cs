55ffa05fbb768b5c3c64611ea617a736a893970f
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-06-07 12:00:21 +0300

    Add files via upload

---
+++ 27.03/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _27._03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zad 1
+            ////zadelqne na pamet
+            //int n = int.Parse(Console.ReadLine()); 
+            //string[] text = new string[n];
+            ////vhod
+            //for(int i = 0; i < text.Length; i++)
+            //{
+            //    text[i] = Console.ReadLine();
+            //}
+            ////izhod
+            //for(int i = 0;i < text.Length; i++)
+            //{
+            //    Console.WriteLine(text[i].Length);
+            //}
+
+            //Zad 2
+            //string arr = Console.ReadLine();
+            //string arr1=Console.ReadLine();
+            //Console.WriteLine(String.Concat(arr," ", arr1));
+
+            //Zad 3
+            //string arr=Console.ReadLine();
+            //for (int i = arr.Length-1; i >= 0; i--)
+            //{
+            //    Console.Write(arr[i]);
+            //}
+          
+            
+
+
+
+        }
+    }
+}
