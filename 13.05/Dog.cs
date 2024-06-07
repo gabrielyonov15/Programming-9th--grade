1abc6eeca231e380f8030f90fd49bc269d23d87b
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-06-07 11:51:37 +0300

    Add files via upload

---
+++ 13.05/Dog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._05
+{
+    internal class Dog
+    {
+        static int dogCount;
+
+        private string name;
+        private int age;
+        public Dog(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            dogCount += 1;
+        }
+        public void Bark()
+        {
+            Console.WriteLine("wow-wow");
+        }
+        public void Bark1()
+        {
+            Console.WriteLine("mql");
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("Dog's name:"+ this.name + "; age:"+ this.age + "; Obajda se sas: ");
+            this.Bark();
+           
+        }
+        public void PrintInfo1()
+
+        {
+            Console.WriteLine("Cat's name:" + this.name + "; age:" + this.age + "; Obajda se sas: ");
+            this.Bark1();
+        }
+    }
+
+    
+}
---
+++ 13.05/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _13._05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog  = new Dog("Sharo", 1);
+            dog.PrintInfo();
+            Dog cat = new Dog("Tom", 1);
+            cat.PrintInfo1();
+        }
+    }
+}
