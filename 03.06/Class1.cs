a611c6fdf63ce08fedbaa2f6c9aa80d3cb9c7d20
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-06-07 11:50:01 +0300

    Add files via upload

---
+++ 03.06/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._06
+{
+    internal class Class1
+    {
+        public string category;
+        public int broi;
+        public int number;
+        public String Category
+        {
+            set { category = value; }
+            get { return category; }
+        }
+        public int Broi
+        {
+            set { broi = value; }
+            get { return broi; }
+        }
+        public int Number
+        {
+            set { number = value; }
+            get { return number; }
+        }
+    }
+}
