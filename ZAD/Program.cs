ba8d29ccc5f9b7f99bbb88ad473dd7fe17918605
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-06-07 12:03:15 +0300

    Add files via upload

---
+++ ZAD/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ZAD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zadelqne na pamet
+            double[] pari = new double[2];
+            string[] vidZakuska = new string[2];  
+            //vhod
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Vivedi pari:");
+                pari[i] = double.Parse(Console.ReadLine());
+                Console.WriteLine("Vivedi zakuska");
+                vidZakuska[i] = Console.ReadLine();
+            }
+            //izhod
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine($"{vidZakuska[i]} / {pari[i]}");
+            }
+
+        }
+    }
+}
