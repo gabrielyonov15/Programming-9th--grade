a29b2621727891673195c5c911311cd93aceef05
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-06-07 11:57:40 +0300

    Add files via upload

---
+++ 24.04/Person.cs
@@ -0,0 +1,68 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Runtime.CompilerServices;
+//using System.Security.Cryptography.X509Certificates;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace _24._04
+//{
+//    internal class Person
+//    {
+//        //private string name;
+//        //public int age;
+//        //private List<BankAccount> bankAccounts;
+//        //public Person(string name, int age)
+//        //{
+//        //    this.name = name;
+//        //    this.age = age;
+//        //    this.bankAccounts = new List<BankAccount>();
+//        //}
+//        //public Person(string name, int age,List<BankAccount> accounts)
+//        //{
+//        //    this.name = name;
+//        //    this.age = age;
+//        //    this.bankAccounts = accounts;
+//        //}
+//        //public double GetBalance()
+//        //{
+//        //    return this.bankAccounts
+//        //}
+//        //        private string name;
+
+//        //        private int age;
+
+//        //        private List<BankAccount> accounts;
+
+//        //        public Person(string name, int age)
+
+//        //        : this(name, age, new List<BankAccount>))
+
+//        //{}
+
+//        //public Person(string name, int age, List<BankAccount> accounts)
+
+//        //        {
+
+//        //            this.name = name;
+
+//        //            this.age = age;
+
+//        //            this.accounts = accounts;
+
+//        //        }
+//        public string name;
+//        public int age;
+//        public String Name
+//        {
+//            get { return this.name; }
+//            set { this.name = value; }
+//        }
+//        public int Age
+//        {
+//            get { return this.age; }
+//            set { this.age = value; }
+//        }
+//    }
+//}
