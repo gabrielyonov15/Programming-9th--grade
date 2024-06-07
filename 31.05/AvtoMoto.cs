108d57f75bb67a4a60b0b571c8cc3c167c516511
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-06-07 12:01:44 +0300

    Add files via upload

---
+++ 31.05/AvtoMoto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31._05
+{
+    internal class AvtoMoto
+    {
+        public string category;
+        public int tires;
+        public int regNumber;
+        //public AvtoMoto(string category, int tires, int regNumber)
+        //{
+        //    this.category = category;
+        //    this.tires = tires;
+        //    this.regNumber = regNumber;
+        //}
+        public String Category
+        { 
+            get { return category; }
+            set {  category = value; }
+        }
+        public int Tires    
+        {
+            get { return tires; }
+            set { tires = value; }
+        }
+        public int RegNumber
+        {
+            get { return regNumber; }
+            set { regNumber = value; }
+        }
+
+        public void IntroduceYourself()
+        {
+            if (tires ==2)
+            {
+                category = "A";
+                Console.WriteLine($"Category:{category} Tires:{tires}");
+            }
+            else
+            {
+                category = "B";
+                Console.WriteLine($"Category:{category} Tires:{tires}");
+            }
+        }
+        public void IntroduceYourself1()
+        {
+            Console.WriteLine($"Category:{category} RegNumber:{regNumber}");
+        }
+
+    }
+}
---
+++ 31.05/Program.cs
@@ -0,0 +1,66 @@
+﻿
+
+namespace _31._05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AvtoMoto A = new AvtoMoto();
+            int n = int.Parse(Console.ReadLine());
+            string[] category = new string[n];
+            int[] tires = new int[n];
+            int[] regNumber = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+               // Console.WriteLine("Category:");
+                category[i] ="A";
+                Console.WriteLine("Tires:");
+                tires[i]=int.Parse(Console.ReadLine());
+                Console.WriteLine("RegNumber:");
+                regNumber[i] = int.Parse(Console.ReadLine());
+                if (tires[i] == 2)
+                {
+                    category[i] = "A";
+                    Console.WriteLine($"Category:{category[i]} Tires:{tires[i]}");
+                    // Console.WriteLine($"Category:{category[i]} RegNumber:{regNumber[i]}");
+                }
+                else
+                {
+                    category[i] = "B";
+                    Console.WriteLine($"Category:{category[i]} Tires:{tires[i]}");
+                    // Console.WriteLine($"Category:{category[i]} RegNumber:{regNumber[i]}");
+                }
+                Console.WriteLine($"Category:{category[i]} RegNumber:{regNumber[i]}");
+                // if (tires[i] == 2)
+                // {
+                //     Console.WriteLine($"Category:{category[i]} Tires:{tires[i]}");
+                // }
+                // else
+                // {
+                //     Console.WriteLine($"Category:{category[i]} Tires:{tires[i]}");
+                // }
+                // //Console.WriteLine($"Category:{category[i]} Tires:{tires[i]}");
+                //// Console.WriteLine($"Category:{category[i]} RegNumber:{regNumber[i]}");
+            }
+            //for(int i =0;i< n; i++)
+            //{
+            //   Console.WriteLine($"Category:{category[i]} RegNumber:{regNumber[i]}");
+            //}
+            //IntroduceYourself();
+            //IntroduceYourself1();
+            //A.IntroduceYourself();
+            //A.IntroduceYourself1();
+        }
+
+        //private static void IntroduceYourself1()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //private static void IntroduceYourself()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
