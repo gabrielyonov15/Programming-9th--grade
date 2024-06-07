46d5a7b3a872cc71034344d9401721e71f29983e
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-06-07 12:01:00 +0300

    Add files via upload

---
+++ 27.05/Artimeties.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27._05
+{
+    internal class Artimeties
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Multiply(int b, int a)
+        {
+            return -(a * b);
+        }
+
+      
+    }
+}
+            
