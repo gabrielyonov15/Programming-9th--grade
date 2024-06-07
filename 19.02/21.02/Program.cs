6278429999e9b258f76748a794d6269325144914
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-06-07 11:54:43 +0300

    Add files via upload

---
+++ 19.02/21.02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _21._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zadelqne na pamet
+            int n = int.Parse(Console.ReadLine());
+            string[] bukvi = new string[n];
+            //vhod
+            for (int i = 0; i < n; i++)
+            {
+                bukvi[i] = Console.ReadLine();
+            }
+            //izhod
+            for (int i = n-1 ; i >= 0; i--)
+            {
+                Console.WriteLine(bukvi[i]);
+            }
+            //izhod2
+            //for (int i = 1; i < 2; i++)
+            //{
+            //    Console.WriteLine(number[i]);
+            //}
+        }
+    }
+}
