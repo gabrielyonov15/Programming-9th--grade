8c93e1bfee7d4e384611cdee8c51297aafeb8193
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-06-17 13:39:52 +0300

    Add files via upload

---
+++ Vakanciq/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace Vakanciq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vakanciq A = new Vakanciq();
+            int n = int.Parse(Console.ReadLine());
+            int[] dniZaPochivki = new int[n];
+            string[] name = new string[n];
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Name:");
+                name[i] = Console.ReadLine();
+                Console.WriteLine("Destinqciq:");
+                A.Destinqciq = Console.ReadLine();
+                Console.WriteLine("DniZaPochivki:");
+                dniZaPochivki[i]=int.Parse(Console.ReadLine());
+                Console.WriteLine("Izpit:");
+                A.Izpit = Console.ReadLine();
+                Console.WriteLine($"Name:{name[i]} Destinqciq:{A.Destinqciq} DniZaPochivka:{dniZaPochivki[i]} Izpit:{A.Izpit}");
+            }
+            Console.WriteLine("Izhod 1");
+            for(int i = 0;i<n;i++)
+            {
+                Console.WriteLine($"Name:{name[i]} Destinqciq:{A.Destinqciq} DniZaPochivka:{dniZaPochivki[i]} Izpit:{A.Izpit}");
+            }
+            Console.WriteLine("Izhod 2");
+            for (int i = 0; i < n;i++)
+            {
+                Console.WriteLine($"Name:{name[i]}-");
+            }
+            Console.WriteLine("Izhod 3");
+            for (int i = 0; i < n; i++)
+            {
+                if(A.Izpit == "Da")
+                {
+                    Console.WriteLine("Kolko izpita imash");
+                    int kolkoIzpit = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Imam {kolkoIzpit} izpita");
+                }
+                else
+                {
+                    Console.WriteLine("Priqtna pochivka");
+                }
+            }
+            Console.WriteLine("Izhod 4");
+            for (int i = 0;i<n;i++) 
+            { 
+                if(A.Destinqciq != "Sopot")
+                {
+                    Console.WriteLine($"{A.Destinqciq}");
+                }
+            }
+            Console.WriteLine("Izhod 5");
+            for (int i = 0;i < name.Length-1;i++)
+            {
+                for(int j = 0; j < name.Length-1;j++)
+                {
+                    if (String.Compare(name[j] , name[j+1])<0)
+                    {
+                        string swapVar = name[j];
+                        name[j] = name[j + 1];
+                        name[j + 1] = swapVar;
+                    }
+                }
+            }
+            for(int i = 0;i<n;i++)
+            {
+                Console.WriteLine(String.Join(" ", name[i]));
+            }
+            Console.WriteLine("Izhod 6");
+            for (int i = 0; i < dniZaPochivki.Length - 1; i++)
+            {
+                for (int j = 0; j < dniZaPochivki.Length - 1; j++)
+                {
+                    if (dniZaPochivki[j] < dniZaPochivki[j + 1])
+                    {
+                        int swapVar = dniZaPochivki[j];
+                        dniZaPochivki[j] = dniZaPochivki[j + 1];
+                        dniZaPochivki[j + 1] = swapVar;
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(String.Join(" ", dniZaPochivki[i]));
+            }
+
+
+
+        }
+    }
+}
---
+++ Vakanciq/Vakanciq.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vakanciq
+{
+    internal class Vakanciq
+    {
+        private string name;
+        private string destinqciq;
+        private int dniZaPochivka;
+        private string izpit;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Destinqciq
+        {
+            get { return destinqciq; }
+            set { destinqciq = value; }
+        }
+        public int DniZaPochivka
+        {
+            get { return dniZaPochivka; }
+            set { dniZaPochivka = value; }
+        }
+        public string Izpit
+        {
+            get { return izpit; }
+            set { izpit = value; }
+        }
+
+    }
+}
